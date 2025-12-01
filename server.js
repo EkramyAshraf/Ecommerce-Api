703e892300666baa471e274dd07b5f33e3bfa8d6
Ekramy Ashraf <ekramyashraf@gmail.com>
2025-12-01 23:49:39 +0200

    just edit package

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 
 app.use(globalErrorHandler);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
